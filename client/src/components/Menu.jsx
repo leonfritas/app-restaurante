f13527697ef7b027716357277ed7593c19591f9b
Leo <leon_fritas@hotmail.com>
2024-07-13 13:57:30 -0400

    Novo botão para voltar durante o processo de realizar pedido.

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
