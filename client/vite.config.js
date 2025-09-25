250cbafd2f88dd51e9d1d40e40d9b5467c261bc8
Leo <leon_fritas@hotmail.com>
2025-09-24 23:26:55 -0400

    viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // 👈 garante que as rotas funcionem em produção
   build: {
     sourcemap: true, // Habilita os source maps
   },
